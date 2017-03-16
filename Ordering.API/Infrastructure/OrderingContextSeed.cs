51e9f13cbd936aea0996e53f99e712139195f4d9
bosxixi <bosxixi@outlook.com>
2017-03-16 17:58:38 +0800

    add ordering api

---
+++ Ordering.API/Infrastructure/OrderingContextSeed.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Ordering.API.Data;
+using Ordering.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.Infrastructure
+{
+    public class OrderingContextSeed
+    {
+        public static async Task SeedAsync(IApplicationBuilder applicationBuilder)
+        {
+            var context = (OrderingContext)applicationBuilder
+                .ApplicationServices.GetService(typeof(OrderingContext));
+
+            using (context)
+            {
+                context.Database.Migrate();
+
+                if (!context.Subscriptions.Any())
+                {
+                    foreach (var s in Subscription.GetAllSubscription())
+                    {
+                        context.Subscriptions.Add(s);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                //if (!context.OrderStatus.Any())
+                //{
+                //    context.OrderStatus.Add(OrderStatus.Canceled);
+                //    context.OrderStatus.Add(OrderStatus.InProcess);
+                //    context.OrderStatus.Add(OrderStatus.Shipped);
+                //}
+
+                await context.SaveChangesAsync();
+            }
+        }
+
+    }
+}
---
+++ Ordering.API/Models/Subscription.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.Models
+{
+    public class Subscription
+    {
+        public string Name { get; set; }
+        public TimeSpan TimeSpan { get; set; }
+        public SubscriptionService SubscriptionService { get; set; }
+        public static IEnumerable<Subscription> GetAllSubscription()
+        {
+            yield return new Subscription { Name = "12 Month Membership", TimeSpan = TimeSpan.FromDays(365 / 1), SubscriptionService = SubscriptionService.Vpn };
+            yield return new Subscription { Name = "6 Month Membership", TimeSpan = TimeSpan.FromDays(365 / 2), SubscriptionService = SubscriptionService.Vpn };
+            yield return new Subscription { Name = "3 Month Membership", TimeSpan = TimeSpan.FromDays(365 / 4), SubscriptionService = SubscriptionService.Vpn };
+            yield return new Subscription { Name = "1 Month Membership", TimeSpan = TimeSpan.FromDays(365 / 12), SubscriptionService = SubscriptionService.Vpn };
+        }
+    }
+}
