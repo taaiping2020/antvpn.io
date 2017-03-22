8f577a375088402375ec4fb447c30b0e470c5b1a
bosxixi <bosxixi@outlook.com>
2017-03-22 23:32:22 +0800

    merging sql server clr

---
+++ Accounting.Sqlserver/LoginRepo.cs
@@ -0,0 +1,80 @@
+using Microsoft.SqlServer.Server;
+using System.IO;
+using System.Net;
+
+namespace Accounting.Sqlserver
+{
+    public class LoginRepo
+    {
+        private readonly string[] loginServers;
+        public LoginRepo(params string[] loginServers)
+        {
+            this.loginServers = loginServers;
+        }
+        public void Apply(Login login, string httpMethod, bool allowFail)
+        {
+            try
+            {
+                foreach (var ls in loginServers)
+                {
+                    var request = (HttpWebRequest)WebRequest.Create(ls + "/api/users");
+                    request.ContentType = "application/json; charset=utf-8";
+                    request.Method = httpMethod;
+
+                    using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                    {
+                        string json = $@"{{""name"": ""{login.LoginName}"",""password"": ""{login.Password}"",""enabled"": {login.Enabled.ToString().ToLower()},""allowDialIn"": {login.AllowDialIn.ToString().ToLower()},""groupName"": ""{login.GroupName}""}}";
+                        streamWriter.Write(json);
+                        streamWriter.Flush();
+                    }
+
+                    var httpResponse = (HttpWebResponse)request.GetResponse();
+                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    {
+                        var result = streamReader.ReadToEnd();
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                if (!allowFail)
+                {
+                    throw ex;
+                }
+            }
+         
+        }
+
+        public void Delete(Login login, bool allowFail)
+        {
+            try
+            {
+                foreach (var ls in loginServers)
+                {
+                    var request = (HttpWebRequest)WebRequest.Create(ls + $"/api/users/{login.LoginName}");
+                    request.ContentType = "application/json; charset=utf-8";
+                    request.Method = "DELETE";
+
+                    using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                    {
+                        streamWriter.Flush();
+                    }
+
+                    var httpResponse = (HttpWebResponse)request.GetResponse();
+                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    {
+                        var result = streamReader.ReadToEnd();
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                if (!allowFail)
+                {
+                    throw ex;
+                }
+            }
+
+        }
+    }
+}
---
+++ Accounting.Sqlserver/SqlTrigger.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.SqlServer.Server;
+using Accounting.Sqlserver;
+
+//example code: https://msdn.microsoft.com/en-us/library/ms131093.aspx
+
+public partial class Triggers
+{
+    private readonly static LoginRepo repo = new LoginRepo("http://dc.antvpn.io:5000");
+
+    // 为目标输入现有表或视图，并取消注释属性行
+    [SqlTrigger(Name = "SqlTrigger", Target = "Logins", Event = "FOR INSERT, UPDATE, DELETE")]
+    public static void SqlTrigger()
+    {
+        using (var connection = new SqlConnection(@"context connection=true"))
+        {
+            connection.Open();
+
+            switch (SqlContext.TriggerContext.TriggerAction)
+            {
+                case TriggerAction.Insert:
+                    {
+                        var command = new SqlCommand(@"SELECT * FROM INSERTED;", connection);
+                        var reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            var inserted = new Login(reader);
+                            repo.Apply(inserted, "POST", false);
+                        }
+
+                        reader.Close();
+                    }
+                   
+                    break;
+                case TriggerAction.Update:
+                    {
+                        var command = new SqlCommand(@"SELECT * FROM INSERTED;", connection);
+                        var reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            var inserted = new Login(reader);
+                            repo.Apply(inserted, "PUT", false);
+                        }
+
+                        reader.Close();
+                    }
+                    
+                    break;
+                case TriggerAction.Delete:
+                    {
+                        var commandDeleted = new SqlCommand(@"SELECT * FROM DELETED;", connection);
+                        var reader = commandDeleted.ExecuteReader();
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                var deleted = new Login(reader);
+                                repo.Delete(deleted, false);
+                            }
+
+                            reader.Close();
+                        }
+                        else
+                        {
+                            SqlContext.Pipe.Send("No rows affected.");
+                        }
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
+
