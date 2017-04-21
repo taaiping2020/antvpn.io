10f83e0c2d60af91332b64f3259ff91a4a04ea58
bosxixi <bosxixi@outlook.com>
2017-04-21 17:45:43 +0800

    fix bug add styles

--- WebMVC/ViewModels/LoginBindingModel.cs
+++ WebMVC/ViewModels/LoginBindingModel.cs
@@ -10,7 +10,8 @@ namespace WebMVC.ViewModels
     public class LoginBindingModel
     {
         [Required]
-        [RegularExpression("[a-zA-Z]+[a-zA-Z0-9]+")]
+        //[RegularExpression("[a-zA-Z]+[a-zA-Z0-9]+")]
+        [RegularExpression("^[a-zA-Z][a-zA-Z0-9]{2,}", ErrorMessage = "The field Username must start with Letter, at least 3 characters, may include number.")]
         [DataType(DataType.Text)]
         public string UserName { get; set; }
         [Required]
